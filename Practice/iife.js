54d474a58fc02703b02e5e2009892eadc52c8932
muhammadnoman710 <nomi32655@gmail.com>
2023-02-22 05:37:57 +0500

    wtf

--- Practice/iife.js
+++ Practice/iife.js
@@ -1,11 +1,11 @@
-(function(){
+(function(message){
 const hero = "batman"
-console.log(hero)
-})();
+console.log(message,hero)
+})("Hello");
 
 //These are "Immediately Invoked Function Expressions" , which provides us private scoping and saves us from variables or function variations
 
-(function(){
+(function(message){
     const hero = "superman"
-    console.log(hero)
-})();
+    console.log(message,hero)
+})("HI");
